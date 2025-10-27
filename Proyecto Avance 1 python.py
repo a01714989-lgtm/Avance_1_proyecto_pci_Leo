74d032a005b8fea12d2454fad125b9bb4f78efaf
a01714989-lgtm <a01714989@tec.mx>
2025-10-26 21:31:00 -0600

    Evidencia 2. Proyecto Integrador
    Cambio final

--- Proyecto Avance 1 python.py
+++ Proyecto Avance 1 python.py
@@ -1,61 +1,44 @@
-#Importamos la libreria math para realizar diferentes operaciones matematicas
-import math
-
 def buscar_en_matriz(codigo_producto, matriz):
+    """Busca un producto en matriz por su código y retorna su información."""
     for fila in matriz:
         if fila[0] == codigo_producto:
-            return fila  # Retorna la lista del producto encontrado
+            return fila
     return None
 
-#FUNCION DE BUSQUEDA DE PORDUCTOS
 
 def productos_busqueda(codigo_producto):
-
-    #CICLO DE BUSQUEDA DE LOS PRODUCTO
-
+    """Procesa la compra de productos y calcula el subtotal, IVA y total."""
+    subtotal = 0
+    
     while True:
-        
         producto = buscar_en_matriz(codigo_producto, matriz_producto)
 
         if producto:
             cantidad = int(input("\nDigite la cantidad de productos: "))
-            print("\nProducto:", producto[1], " Cantidad:", cantidad, " Precio Unitario: $", producto[2])
-            subtotal = producto[2] * cantidad
+            print("\nProducto:", producto[1], " Cantidad:", cantidad,
+                  " Precio Unitario: $", producto[2])
+            subtotal += producto[2] * cantidad
+            desea_continuar = input("\n¿Desea añadir otro producto? (Si/No): ")
+            if desea_continuar.lower() == "no":
+                break
         else:
             print("\nEl código no coincide con ningún producto\n")
-            break
 
-        dc = input("\n¿Desea añadir otro producto? (Si/No): ")
-        
-        if dc.lower() == "no":
-            break
-        
         codigo_producto = int(input("\nDigite el codigo del producto: "))
 
     print("El subtotal es: $", subtotal)
 
-    # Cálculo del IVA
     iva = subtotal * 0.16
-
-    # Cálculo de factura total
     total_factura = subtotal + iva
 
     print("\nIVA: $", iva, "\nTotal: $", total_factura)
 
 
-#Establecemos los productos disponibles(listas)
-
 producto_1 = [1234567890123, "Sandwich Helado Clasico", 28]
-
 producto_2 = [4829301756248, "Takis Fuego Morados", 25.5]
-
 producto_3 = [7601938421576, "Chokis Rojas", 20.5]
-
 producto_4 = [5092846173921, "Electrolit lima limon", 32.5]
-
 producto_5 = [1837562094853, "Trident Negros chicles", 18]
-
-#Establecemos la matriz
 
 matriz_producto = [
     producto_1,
@@ -65,10 +48,6 @@ matriz_producto = [
     producto_5
 ]
 
-#Establecemos el parametro para la funcion
-
 codigo_producto = int(input("\nDigite el codigo del producto: "))
 
-
-#Llamamos a la funcion incluyendo el parametro selecionado
 productos_busqueda(codigo_producto)
